2b6e84748b97f29dcc8a4d2c687a1fe5b3148ee3
laiagc892 <laiagc892@gmail.com>
2021-04-01 12:28:00 +0200

    requirements 1-4 completed

--- app_solved-cool.js
+++ app_solved-cool.js
@@ -1,9 +1,14 @@
+// API Url
+const url = 'http://ec2-35-181-5-201.eu-west-3.compute.amazonaws.com:8080';
+const idTeam = 'spiders';
+
 //Product Constructor
 class Product {
-    constructor(name, price) {
+    constructor(name, price, year, id=Math.random()) {
         this.name = name;
         this.price = price;
         this.year = year;
+        this.id = id
     }
 }
 
@@ -19,7 +24,7 @@ class UI {
         <h5><strong>${product.name}</strong></h5>
         <strong>Price</strong>: ${product.price}€
         <strong>Year</strong>: ${product.year}
-        <a href="#" onclick="UI.deleteProduct(event)" class="dlt btn btn-danger ml-5" name="delete">Delete</a>
+        <a href="#" id="${product.id}" onclick="UI.deleteProduct(event)" class="dlt btn btn-danger ml-5" name="delete">Delete</a>
         </div>
         </div>
         `;
@@ -31,9 +36,11 @@ class UI {
     }
     
     static deleteProduct(event) {
-        console.log("event", event)
+        console.log("event", event.target.id)
         event.target.closest("div.card.text-center.mb-4").remove();
         UI.showMessage("Product removed successfully", "danger");
+
+        deleteProductFromDatabase(event.target.id);
     }
     
     static showMessage(message, cssClass) {
@@ -72,4 +79,65 @@ document.getElementById("product-form").addEventListener("submit", e => {
     UI.showMessage("Product added successfully", "success");
     
     e.preventDefault();
+
+    uploadProductToDatabase(product);
 });
+
+
+// GET
+
+fetch(`${url}/list-products/${idTeam}`, {
+    method: 'GET', // So, we can specify HTTP Methods here. Uh, interesting.
+    headers: { 'Content-Type': 'application/json' }, // Type of data to retrieve. 
+    mode: 'cors', // What is CORS?? https://developer.mozilla.org/es/docs/Web/HTTP/CORS 
+})
+.then(response => response.json())
+.then(info => {
+    const products = info;
+    console.log(products);
+    products.forEach(element => {
+        const productN = new Product(element.title, element.price, element.year, element.id);
+        if (productN.year <= new Date().getFullYear()) {
+            UI.addProduct(productN);
+        }
+    })
+});
+
+
+// POST
+
+function uploadProductToDatabase(product) {
+    fetch(`${url}/add-product/${idTeam}`, {
+    method: "POST",
+    body: JSON.stringify({
+        title: product.name,
+        price: product.price,
+        year: product.year
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+    .then(response => response.json())
+    .then(infoJson => {
+        product.id = infoJson.id;
+        console.log(product)
+    });
+
+    console.log("post-test");
+}
+
+
+// DELETE
+
+function deleteProductFromDatabase(idProduct) {
+    fetch(`${url}/add-product/${idTeam}/${idProduct}`, {
+    method: "DELETE",
+    })
+    .then(response => response.json())
+    .then(infoJson => {
+        console.log(infoJson)
+    });
+
+    console.log("delete-test");
+}
